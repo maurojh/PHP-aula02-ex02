8a6fb445017b15793ae36b996beb7c275301584b
Mauro <maurojh@gmail.com>
2019-08-13 19:05:40 -0300

    Teste 2

---
+++ a02ex01.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <title>Dados</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-content">
+                <div class="content">
+                    <div class="columns">
+                        <div class="column">
+                            <figure class="image is-128x128">
+                                <?php
+$numero = 5;
+                       
+echo "<img src='img/dado$numero.png'>";      
+?>
+                            </figure>
+                        </div>
+                        <div class="column">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
